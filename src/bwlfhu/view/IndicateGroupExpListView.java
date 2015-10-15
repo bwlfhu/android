ecf7db2de99770ee3288abf6b6bab4418327617b
bwlfhu <bwlfhu@163.com>
2015-10-15 17:14:11 +0800

    Update IndicateGroupExpListView.java

--- src/bwlfhu/view/IndicateGroupExpListView.java
+++ src/bwlfhu/view/IndicateGroupExpListView.java
@@ -62,10 +62,9 @@ public class IndicateGroupExpListView extends ExpandableListView implements AbsL
         }
 
         long pos = getExpandableListPosition(nPos);
-        int childPos = ExpandableListView.getPackedPositionChild(pos);// 获取第一行child的id
+        int childPos = ExpandableListView.getPackedPositionChild(pos);
         if (childPos == AdapterView.INVALID_POSITION)
         {
-            // 第一行不是显示child,就是group,此时没必要显示指示器
             View groupView = getChildAt(nPos - getFirstVisiblePosition());
             indicatorGroupHeight = groupView.getHeight();
             indicatorGroup.setVisibility(groupView.getTop() == 0 ? View.GONE : View.VISIBLE);
@@ -80,11 +79,10 @@ public class IndicateGroupExpListView extends ExpandableListView implements AbsL
             return;
         }
 
-        int groupPos = ExpandableListView.getPackedPositionGroup(pos);// 获取第一行group的id
+        int groupPos = ExpandableListView.getPackedPositionGroup(pos);
         boolean groupExp = isGroupExpanded(groupPos);
         if (groupPos != indicatorGroupId || indicatorGroupExpanded != groupExp)
         {
-            // 如果指示器显示的不是当前group
             indicatorGroupId = groupPos;
             indicatorGroupExpanded = groupExp;
             ExpandableListAdapter adapter = getExpandableListAdapter();
@@ -108,16 +106,15 @@ public class IndicateGroupExpListView extends ExpandableListView implements AbsL
             return;
         }
 
-        // calculate point (0,indicatorGroupHeight) 下面是形成往上推出的效果
         int showHeight = indicatorGroupHeight;
-        int nEndPos = pointToPosition(0, indicatorGroupHeight);// 第二个item的位置
+        int nEndPos = pointToPosition(0, indicatorGroupHeight);
         if (nEndPos == AdapterView.INVALID_POSITION)
         {
             return;
         }
 
         long pos2 = getExpandableListPosition(nEndPos);
-        int groupPos2 = ExpandableListView.getPackedPositionGroup(pos2);// 获取第二个group的id
+        int groupPos2 = ExpandableListView.getPackedPositionGroup(pos2);
         if (groupPos2 != indicatorGroupId)
         {
             View viewNext = getChildAt(nEndPos - getFirstVisiblePosition());
